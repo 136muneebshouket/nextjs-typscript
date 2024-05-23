e05c710d6a0aec0e15e7f793da11214d2789a8ef
Muneeb <muneeb@bryxo.com>
2024-05-23 23:42:29 +0500

    changes no 2

--- nextjs-vps/app/api/tscript/route.ts
+++ nextjs-vps/app/api/tscript/route.ts
@@ -1,6 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from 'next/server'
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { NextResponse } from 'next/server'
 
-export function GET(req: NextApiRequest, res: NextApiResponse) {
-    return NextResponse.json({ name: 'John Doe' })
+// export function GET(req: NextApiRequest, res: NextApiResponse) {
+//     return NextResponse.json({ name: 'John Doe' })
+// }
+
+export const dynamic = 'force-dynamic' // defaults to auto
+export async function GET(request: Request) {
+    return Response.json({ name: 'John Doe' })
 }
